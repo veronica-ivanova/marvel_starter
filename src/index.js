1b8b27d463076e75a9aff251098f40efa2f7a391
veronica-ivanova <veronica.ivanova27@gmail.com>
2025-03-04 20:07:50 +0300

    add RandomChar and transform data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
+// import MarvelService from "./services/MarvelService";
 
 import "./style/style.scss";
 
-const marvelService = new MarvelService();
+// const marvelService = new MarvelService();
 
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
+// marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 const container = document.getElementById("root");
 const root = createRoot(container);
