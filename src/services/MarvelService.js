acbcc9a63ed201bb4de01c91f407e665f8c156d9
veronica-ivanova <veronica.ivanova27@gmail.com>
2025-06-20 15:22:32 +0300

    add CharSearchForm

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -28,6 +28,11 @@ const useMarvelService = () => {
         const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
         return _transformComic(res.data.results[0]);
     }
+
+    const getCharacterByName = async (name) => {
+        const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+        return res.data.results.map(_transformCharacter);
+    }
     const _transformCharacter =(char) => {
         return {
             id: char.id,
@@ -53,7 +58,7 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComic}
+    return {loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComic, getCharacterByName}
 }
 
 export default useMarvelService;
