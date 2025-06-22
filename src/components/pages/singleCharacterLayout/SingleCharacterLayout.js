197a16966854f1ecad8ebd9b22e5b1633bb813b3
veronica-ivanova <veronica.ivanova27@gmail.com>
2025-06-22 16:12:22 +0300

    add dynamic titles and meta tags using React Helmet

--- src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
+++ src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
@@ -1,9 +1,17 @@
+import {Helmet} from "react-helmet";
 import './singleCharacterLayout.scss';
 
 const SingleCharacterLayout = ({data}) => {
     const {name, description, thumbnail} = data;
     return (
         <div className="single-comic">
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`${name} comics book`}
+                />
+                <title>{name}</title>
+            </Helmet>
             <img src={thumbnail} alt={name} className="single-comic__char-img"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{name}</h2>
